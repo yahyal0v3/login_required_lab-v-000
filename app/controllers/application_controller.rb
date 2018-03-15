d5a2a6c13d033a497ba4c28a084c266de1fb39fa
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-15 01:43:10 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-    def current_user
-      session[:name]
-    end
+  def current_user
+    session[:name]
+  end
 
 end
